27cb4d8de3e0ee94977db10c21b6cc865d6b1eae
Michael Robinette <mrobine2@Jhu.edu>
2020-03-20 14:26:12 -0400

    It's all functional and working. Current state: bounce between an aggressive policy (mine) and a conservative policy (Stockfish)

--- rc_scratchpad.py
+++ rc_scratchpad.py
@@ -17,7 +17,7 @@ class MichaelBot(Player):
         self.turn_num = 0  # to be iterated for turn-based decisions
         self.piece_values = {1: 1, 2: 3, 3: 3.5, 4: 5, 5: 9, 6: 2}
         self.enemy_values = {1: 1, 2: 3, 3: 3.5, 4: 5, 5: 9, 6: 100}  # priorities for attacking
-        self.in_out_timer = 0  # to say if we need to attack quickly then retreat
+        self.game_state = 1  # to say if we need to attack quickly then retreat
 
     def handle_game_start(self, color: Color, board: chess.Board, opponent_name: str):
         self.board = board
@@ -32,7 +32,7 @@ class MichaelBot(Player):
     def _get_my_pieces(self, move_actions):
         pt_dict = {'P': 1, 'N': 2, 'B': 3, 'R': 4, 'Q': 5, 'K': 6}
 
-        #find all moves
+        # find all moves
         positions = []
         piece_types = []
         targets = []
@@ -46,6 +46,7 @@ class MichaelBot(Player):
             piece_types.append(pt_dict[self.board.piece_at(i.from_square).symbol().upper()])
         piece_dict = {'position': positions, 'pieces': piece_types, 'targets': targets}
         return piece_dict
+
     def _calc_moves(self, piece_dict):
         # where I do Gillespie-esque decision making:
 
@@ -56,11 +57,30 @@ class MichaelBot(Player):
             positions = [64 - x for x in positions]
         distance_scores = [3 * int(x/8) for x in positions]  # weighting distance from the start
         centrality = [-1 * abs(4.5 - x % 8) for x in positions]  # weighting pieces near the center of the board
-        piece_scores = [self.piece_values[p] for p in piece_dict['pieces']]
+        piece_scores = [1.5*self.piece_values[p] for p in piece_dict['pieces']]
         total_score = [x + y + z for x, y, z in zip(distance_scores, piece_scores, centrality)]  # these may need to be fractions for random.choices
-        piece_to_move = random.choices(piece_dict['position'], weights=total_score)[0]
-
+        idx_to_move = random.choices(range(len(piece_dict['position'])), weights=total_score)[0]
+        piece_to_move = piece_dict['position'][idx_to_move]
+        # can probably just find piece_to_move as before. idx_to_move may not be important. Leave as-is for now
         return piece_to_move
+
+    def _find_target(self, piece_to_move, piece_dict):
+
+        targets = piece_dict['targets']
+        positions = piece_dict['position']
+        idxs = [i for i, j in enumerate(positions) if j == piece_to_move] # getting indices of all possible moves
+        my_targets = [targets[i] for i in idxs]
+
+        # naive: just pick the largest distance (note - this will bias the right side of the board)
+        # this models the aggressive side of the behavior - when using this policy, we can only move forward (as aggressively as possible)
+        if self.color == chess.BLACK:
+            target_dists = [(piece_to_move - i) for i in my_targets]
+        elif self.color == chess.WHITE:
+            target_dists = [(i - piece_to_move) for i in my_targets]
+        max_target_idx = target_dists.index(max(target_dists))
+        choice_target = my_targets[max_target_idx]
+        return choice_target
+
 
 
     def choose_sense(self, sense_actions: List[Square], move_actions: List[chess.Move], seconds_left: float) -> \
@@ -68,9 +88,13 @@ class MichaelBot(Player):
         # return random.choice(sense_actions)
         #print('senses: ', sense_actions)
         #print('actions: ', (move_actions))
-        print(self._get_my_pieces(move_actions))
-        print(' ')
-        print(self._calc_moves(self._get_my_pieces(move_actions)))
+        #print(self._get_my_pieces(move_actions))
+        #print(' ')
+        #print(self._calc_moves(self._get_my_pieces(move_actions)))
+
+
+
+
         # if we might capture a piece when we move, sense where the capture will occur
         future_move = self.choose_move(move_actions, seconds_left)
         if future_move is not None and self.board.piece_at(future_move.to_square) is not None:
@@ -88,8 +112,8 @@ class MichaelBot(Player):
             self.board.set_piece_at(square, piece)
 
     def choose_move(self, move_actions: List[chess.Move], seconds_left: float) -> Optional[chess.Move]:
-        # return random.choice(move_actions + [None])
-        ### keep this
+        # Bounce between an aggressive policy (mine) and a conservative policy (Stockfish)
+
         # if we might be able to take the king, try to
         enemy_king_square = self.board.king(not self.color)
         if enemy_king_square:
@@ -100,18 +124,25 @@ class MichaelBot(Player):
                 return chess.Move(attacker_square, enemy_king_square)
 
         ### my code here
-
-
+        if self.game_state == 1:
+            self.game_state *= -1
+            piece_dict = self._get_my_pieces(move_actions)
+            piece_to_move = self._calc_moves(piece_dict)
+            target = self._find_target(piece_to_move, piece_dict)
+            #self.game_state *= -1
+            return chess.Move(piece_to_move, target)
         # otherwise, try to move with the stockfish chess engine
-        try:
-            self.board.turn = self.color
-            self.board.clear_stack()
-            result = self.engine.play(self.board, chess.engine.Limit(time=0.5))
-            return result.move
-        except chess.engine.EngineTerminatedError:
-            print('Stockfish Engine died')
-        except chess.engine.EngineError:
-            print('Stockfish Engine bad state at "{}"'.format(self.board.fen()))
+        elif self.game_state == -1:
+            try:
+                self.board.turn = self.color
+                self.board.clear_stack()
+                result = self.engine.play(self.board, chess.engine.Limit(time=0.5))
+                self.game_state *= -1
+                return result.move
+            except chess.engine.EngineTerminatedError:
+                print('Stockfish Engine died')
+            except chess.engine.EngineError:
+                print('Stockfish Engine bad state at "{}"'.format(self.board.fen()))
 
         # if all else fails, pass
         return None
